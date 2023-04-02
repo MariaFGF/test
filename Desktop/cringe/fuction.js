c013466bcb0bc502db943c663bf9f8bcf8951338
MariaFGF <122865299+MariaFGF@users.noreply.github.com>
2023-04-02 23:25:11 +0300

    Add files via upload

---
+++ Desktop/cringe/fuction.js
@@ -0,0 +1,69 @@
+//1
+/*let mass1 = [7, 8, 10, 40, 50]
+let mass2 = []
+    for (let i = 0; i < mass1.length; i++) {
+    if (isNumberInRange(mass1[i])) {
+    mass2.push(mass1[i])
+    }
+}
+console.log(mass2)
+    function isNumberInRange (num) {
+    return num > 0 && num < 10
+}*/
+
+
+//2
+/*function agePeople(age) {
+    if(age < 18){
+        console.log(age, 'ребенок')  
+    }
+    else if(age < 31) {
+        console.log(age, 'молодой')
+    }
+    else if(age < 56) {
+        console.log(age,  'зрелый')
+    }
+    else if(age > 55) {
+        console.log(age, 'старый')
+    }
+}
+for (let age = 0; age < 101; age++) {
+    agePeople(age)
+}*/
+
+
+//3
+
+/*let mass1 = [4, " ", null, false, undefined, 2, -2, 24, 0, 5, -1, 30]
+
+function isNumberInRange(num) {
+	if (num != false && num != ' ' && num != undefined && num != 0 && num !=null) {
+		return true
+	} else {
+		return false
+	}
+}
+let mass2 = []
+for (let i = 0; i <= mass1.length; i++) {
+
+	if (isNumberInRange(mass1[i])) {
+
+		mass2.push(mass1[i])
+
+	}
+}
+console.log(mass2)*/
+
+
+//4
+/*let mass1 = function number(a) {
+    if(!mass2.includes(a)) {
+        mass2.push(a)
+    } 
+}
+let mass3 = ['f', 7, 'f', 7, 7, 3, 'c']
+let mass2 = []
+mass3.forEach(mass1)
+console.log(mass2)*/
+
+
